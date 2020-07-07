7f626b6e0eaf4720d64c775c5387f03d8b71829d
Sonali-Pansande <sonali.sadigale18@gmail.com>
2020-07-07 16:55:36 +0530

    swagger changes

---
+++ Web_App_Local/Controllers/CategoryAPIController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_App_Local.Models;
+using Web_App_Local.Services;
+
+namespace Web_App_Local.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryAPIController : ControllerBase
+    {
+        private readonly IRepository<Category, int> catRepo;
+
+        public CategoryAPIController(IRepository<Category, int> catRepo)
+        {
+            this.catRepo = catRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var cats = await catRepo.GetAsync();
+            return Ok(cats);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var cats = await catRepo.GetAsync(id);
+            return Ok(cats);
+        }
+
+        [HttpPost("{CategoryId}/{CategoryName}/{BasePrice}")]
+        public async Task<IActionResult> Post([FromRoute]Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                var cats = await catRepo.CreateAsync(cat);
+                return Ok(cats);
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put( int id, [FromBody] Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                var cats = await catRepo.UpdateAsync(id, cat);
+                return Ok(cats);
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cats = await catRepo.DeleteAsync(id);
+            return Ok(cats);
+        }
+    }
+}
--- Web_App_Local/Startup.cs
+++ Web_App_Local/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Web_App_Local.CustomFilters;
 using Web_App_Local.Data;
 using Web_App_Local.Models;
@@ -54,6 +55,13 @@ namespace Web_App_Local
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            // configure the swagger service
+            services.AddSwaggerGen(C => 
+                {
+                    C.SwaggerDoc("v1",
+                        new OpenApiInfo { Title = "ASP.NET Core API", Version = "V1" });
+                });
+
             services.AddScoped<IRepository<Category, int>, CategoryRepository>();
 
             services.AddScoped<IRepository<Product, int>, ProductRepository>();
@@ -65,6 +73,12 @@ namespace Web_App_Local
             {
                 options.Filters.Add(typeof( CustomExceptionFilter));
             });
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                });
             services.AddRazorPages();
 
         }
@@ -83,6 +97,16 @@ namespace Web_App_Local
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+
+            // configure the swagger middleware
+            app.UseSwagger();
+
+            // provide / respond the HTML UI
+            app.UseSwaggerUI(C =>
+            {
+                C.SwaggerEndpoint("/swagger/v1/swagger.json", "ASP.NET Core API Documentation");
+            });
+
             app.UseStaticFiles();
 
             app.UseRouting();
